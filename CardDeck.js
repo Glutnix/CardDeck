68ebc5f48a2083a8801c8154bf6a0666706f910a
Brett Taylor <brett@webfroot.co.nz>
2014-03-31 17:08:41 +1300

    First code commit

---
+++ CardDeck.js
@@ -0,0 +1,113 @@
+var CardDeck = function (settings) {
+	// constructor
+
+	// defaults
+	var defaults = {
+		decks: 1,
+		handSize: 5,
+		ranks: "2,3,4,5,6,7,8,9,10,Jack,Queen,King,Ace".split(","),
+		suits: "Clubs,Diamonds,Hearts,Spades".split(","),
+		jokers: ["Black","Red"]
+	};
+
+	this.theDeck = [];
+
+	this.settings = this.extend({}, defaults, settings);
+
+	return this.initialize();
+};
+
+CardDeck.prototype.initialize = function () {
+	this.populateDeck();
+	this.sort();
+	return this;
+};
+
+CardDeck.prototype.populateDeck = function() {
+	this.theDeck = [];
+	for (var i = 0; i < this.settings.decks; i++) {
+		var deck = this.createDeck(i);
+		this.theDeck = this.theDeck.concat(deck);
+	}
+};
+
+CardDeck.prototype.createDeck = function (deckID) {
+	deckID = deckID || 0;
+	var i;
+	var deck = [];
+	var decklength = this.settings.ranks.length * this.settings.suits.length;
+	for (i = 0; i < decklength; i += 1) {
+		var rank = i % this.settings.ranks.length;
+		var suit = i % this.settings.suits.length;
+		var card = new CardDeck.Card(rank, suit, this, deckID);
+
+		deck.push(card);
+	}
+	for (i = 0; i < this.settings.jokers.length; i++) {
+		deck.push(new CardDeck.Card(i, this.settings.suits.length, this, deckID));
+	}
+
+	return deck;
+};
+
+CardDeck.prototype.shuffle = function() {
+	// based on http://bost.ocks.org/mike/shuffle/
+	var m = this.theDeck.length;
+	var t, i;
+
+	// While there remain elements to shuffle
+	while (m) {
+
+		// Pick a remaining element
+		i = Math.floor(Math.random() * m--);
+
+		// And swap it with the current element.
+		t = this.theDeck[m];
+		this.theDeck[m] = this.theDeck[i];
+		this.theDeck[i] = t;
+	}
+
+	return this;
+};
+
+CardDeck.prototype.sort = function () {
+	this.theDeck.sort(function deckSort(a, b) {
+		if (a.suitIndex !== b.suitIndex) {
+			return a.suitIndex - b.suitIndex; // > is T/F, T = 1, F = 0
+		} else {
+			// suits do match, so sort by rank
+			return a.rankIndex - b.rankIndex;
+		}
+	});
+};
+
+
+CardDeck.prototype.dealHand = function (size) {
+	size = size || this.settings.handSize;
+	var hand = [];
+	for (var i = 0; i < size; i += 1) {
+		var card = this.theDeck.pop();
+		hand.push(card);
+	}
+	return hand;
+};
+
+CardDeck.prototype.extend = function () {
+	for(var i=1; i<arguments.length; i++) {
+		for(var key in arguments[i]) {
+			if(arguments[i].hasOwnProperty(key)) {
+				arguments[0][key] = arguments[i][key];
+			}
+		}
+	}
+	return arguments[0];
+};
+
+CardDeck.Card = function (rankIndex, suitIndex, deck, deckID) {
+	var joker = suitIndex == deck.settings.suits.length;
+	this.rankIndex = rankIndex;
+	this.rank = (joker ? deck.settings.jokers[rankIndex] : deck.settings.ranks[rankIndex]) || "Blank";
+	this.suitIndex = suitIndex;
+	this.suit = deck.settings.suits[suitIndex] || (joker ? "Joker" : "");
+	this.deckID = deckID;
+};
--- README.md
+++ README.md
@@ -2,3 +2,10 @@ CardDeck
 ========
 
 A set of JavaScript objects that can be used to simulate stacks of playing cards.
+
+
+Goals
+-----
+
+* To make usable objects that could be used to create solitare or multiplayer playing card games
+* To make JavaScript objects that demonstrate making classes of objects using JS prototypes
